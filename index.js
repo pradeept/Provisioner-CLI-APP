402a2db892c9cb60e8b28a02468f1dca1585f96c
pradeept-sirpi <pradeept@sirpi.io>
2024-10-21 17:56:42 +0530

    greet added

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import chalk from "chalk";
-
-console.log(chalk.red('Hello'))
-
+import figlet from "figlet";
+import * as emoji from 'node-emoji'
 
 
 // Greet
+
+console.log(
+    figlet.textSync("Rstudio Provisioner!", {
+      font: "Standard",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 100,
+    //   whitespaceBreak: true,
+    })
+  );
+
+  console.log(chalk.bgGreen.bold("Hi There!"))
+  console.log(emoji.find(':pizza'))
 // List available r versions
 // ask for the password to set for the rstudio
 // show spinner - untill provisioned
 // Give back the URL
-
