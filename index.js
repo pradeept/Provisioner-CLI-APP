5c4694e5e413a5164a25d05a771b4d54aa00a3d0
pradeept-sirpi <pradeept@sirpi.io>
2024-10-21 17:35:00 +0530

    initial code

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(chalk.red('Hello'))
+
+
+
+// Greet
+// List available r versions
+// ask for the password to set for the rstudio
+// show spinner - untill provisioned
+// Give back the URL
+
